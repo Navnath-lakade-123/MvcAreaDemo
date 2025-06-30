347648a05fd1667123c581efcd7d988e8574bfb8
Navnath-lakade-123 <ndlakade@gmail.com>
2025-06-30 15:56:57 +0530

    comment added

--- AreasWithUserWiseDynamicMenus/Controllers/AccountController.cs
+++ AreasWithUserWiseDynamicMenus/Controllers/AccountController.cs
@@ -13,6 +13,7 @@ namespace AreasWithUserWiseDynamicMenus.Controllers
         }
         public IActionResult Login()
         {
+            //my new comment
             return View();
         }
 
--- AreasWithUserWiseDynamicMenus/obj/Debug/net8.0/AreasWithUserWiseDynamicMenus.AssemblyInfo.cs
+++ AreasWithUserWiseDynamicMenus/obj/Debug/net8.0/AreasWithUserWiseDynamicMenus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AreasWithUserWiseDynamicMenus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3381fb35d1ea6dafa61eeff01417d33dcbbbbf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AreasWithUserWiseDynamicMenus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AreasWithUserWiseDynamicMenus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
